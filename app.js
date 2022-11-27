59faed46e3feb3020764782a0351b75f29fe9e18
JuanEs02 <114095229+JuanEs02@users.noreply.github.com>
2022-11-26 21:32:39 -0500

    Inicio de Cards y creación de api prueba

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const productos = document.getElementById('productos')
+const fragment = document.createDocumentFragment()
 
-const fetchData = async () =>{
+document.addEventListener('DOMContentLoaded', e => { fetchData() })
+const fetchData = async () => {
     const rest = await fetch('api.json')
     const data = await rest.json()
+    pintarCards(data);
 }
 
-const pintarCards = data =>{
-    console.log(data);
+const pintarCards = data => {
+    const templateProductos = document.getElementById("template-productos").content
+
+    data.forEach(item => {
+        templateProductos.querySelector('h5').textContent = item.titulo;
+        templateProductos.querySelector('p').textContent = item.precio;
+        templateProductos.querySelector('img').setAttribute("src", item.imagen)
+        templateProductos.querySelector('button').dataset.id = item.id;
+        const clone = templateProductos.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    productos.appendChild(fragment)
 }
